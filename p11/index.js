ab8e75fe527ba4b5c3f3995f0303855e9662cd33
ryujia <yujia529@gmail.com>
2016-05-23 12:32:49 +0900

    commit

---
+++ p11/index.js
@@ -0,0 +1,34 @@
+$(function(){
+
+	slideNum = 1;
+	$(".slideBtnUl > .leftBtn").click(function(){
+		prev();
+	})
+	$(".slideBtnUl > .rightBtn").click(function(){
+		next();
+	})
+
+	function prev(){
+		slideNum-=1;
+		if( slideNum == 0 )
+		{
+			slideNum=2;
+			$(".txtUl").css("left",-200+"%");
+			$(".mainSlder > .bg").css("left",-200+"%");
+		}
+			$(".txtUl").stop().animate({"left":(slideNum-1)*-100+"%"});
+				$(".mainSlder > .bg").stop().animate({"left":(slideNum-1)*-100+"%"});
+	}
+	function next(){
+		slideNum+=1;
+		if( slideNum==4 )
+		{
+			slideNum=2;
+			$(".txtUl").css("left",0);
+			$(".mainSlder > .bg").css("left",0);
+		}
+		$(".txtUl").stop().animate({"left":(slideNum-1)*-100+"%"});
+		$(".mainSlder > .bg").stop().animate({"left":(slideNum-1)*-100+"%"});
+
+	}
+})
